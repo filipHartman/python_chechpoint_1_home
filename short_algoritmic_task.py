28a4bc800b4ce083d3f50091570fa557c0e2b6cb
filipHartman <filip.hartman23@gmail.com>
2017-09-09 13:18:14 +0200

    Add function that print numbers on specific conditions

--- short_algoritmic_task.py
+++ short_algoritmic_task.py
@@ -1,4 +1,11 @@
+def print_three_digit_numbers():
+    for number in range(100, 1000):
+        if number % 7 == 0 or number % 9 == 0:
+            print(number)
+
+
 def main():
-    pass
-if __name__ == 'main':
+    print_three_digit_numbers()
+
+if __name__ == '__main__':
     main()
