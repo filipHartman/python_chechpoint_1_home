ab3871f8fcf6eab89d9babc80f5558530e2586be
filipHartman <filip.hartman23@gmail.com>
2017-09-09 21:23:49 +0200

    Initalize functions

---
+++ todo.py
@@ -0,0 +1,21 @@
+def listing_tasks():
+    pass
+
+
+def add_task():
+    pass
+
+
+def mark_as_completed():
+    pass
+
+
+def archive_completed_tasks():
+    pass
+
+
+def main():
+    pass
+
+
+main()
